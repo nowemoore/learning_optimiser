68484ba85f13f8019eac889bc489d555401c6b05
nowemoore <nowe.moore@gmail.com>
2025-10-04 14:57:24 +0100

    updated user class

--- user.py
+++ user.py
@@ -7,6 +7,7 @@ class Learner:
     target_lang: str
     primary_lang: str
     all_langs: list
+    user_data: list = None
     
     def __init__(self, id: int, age: int, target_lang: str, primary_lang: str, all_langs: list):
         self.id = id
@@ -18,7 +19,28 @@ class Learner:
     def __str__(self):
         return "Learner(ID: %s, Age: %s, Target Language: %s, Primary Language: %s, Other Languages: %s)" % (self.id, self.age, self.target_lang, self.primary_lang, self.all_langs)
     
+    def assess_user(self, test_words: list):
+        known_words = []
+        unknown_words = []
+        
+        for word in test_words:
+            response = input(f"Do you know the word '{word}'? (y/n): ").strip().lower()
+            if response == 'y':
+                known_words.append(word)
+            elif response == 'n':
+                unknown_words.append(word)
+            else:
+                print("Invalid input. Please enter 'y' for yes or 'n' for no.")
+                
+        self.set_user_data(known_words, unknown_words)
     
+    def set_user_data(self, known_words: list, unknown_words: list):
+        knowledge_binary = [(word, 1) for word in known_words] + [(word, 0) for word in unknown_words]
+        user_data = [(input, output) for input, output in knowledge_binary] 
+        self.user_data = user_data
+            
+            
+        
     
         
     
